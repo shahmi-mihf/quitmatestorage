9eab916c4e957ce443a3d12108865da43f3c5977
shahmi-mihf <ahmadshahmiasaad@gmail.com>
2026-01-21 11:48:06 +0800

    Add new routes for location, money, calendar, and leaderboard

--- main.dart
+++ main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:quitemate/calendar.dart';
+import 'package:quitemate/leaderboard.dart';
+import 'package:quitemate/location.dart';
 import 'package:quitemate/login.dart';
+import 'package:quitemate/money.dart';
 import 'package:quitemate/profile.dart';
 import 'firebase_options.dart';
 import 'package:quitemate/home.dart';
@@ -17,6 +21,10 @@ void main() async {
       '/register': (context) => const Register(),
       '/login': (context) => const Login(),
       '/profile': (context) => const Profile(),
+      '/location': (context) => const Location(),
+      '/money': (context) => const Money(),
+      '/calendar': (context) => const Calendar(),
+      '/leaderboard': (context) => const Leaderboard(),
 
     },
   ));
